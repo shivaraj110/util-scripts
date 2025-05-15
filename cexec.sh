5c9986fd7819058f3040bf5b1eade68b74162fd2
shivaraj110 <shivarajchandaragi9@gmail.com>
2025-05-15 21:31:00 +0530

    removing binaries after 3 mins after execution

--- cexec.sh
+++ cexec.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 gcc -o cexec $1
-./cexec $2 $3
-sleep 300 && rm -rf cexec && notify-send "removed binary file"
+./cexec $2 $3 &
+sleep 180
+rm -rf cexec && notify-send "removed binary file"
